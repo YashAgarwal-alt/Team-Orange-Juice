7239dbfe7a29ad8f0c43020fb735f4f6f03e3526
YashAgarwalalt <>
2021-05-29 00:39:26 +0000

    nae nae

--- database.py
+++ database.py
@@ -1,15 +1,41 @@
 import sqlite3
 from sqlite3 import IntegrityError
 
-con = sqlite3.connect("users.db")
 
-cur = con.cursor()
 
 def createUser(username, password):
+  con = sqlite3.connect("users.db")
+  cur = con.cursor()  
   try: 
     cur.execute("""
-      insert into Users (username, password) values (?, ?)
+      insert into Users (Username, Password) values (?, ?)
     """, (username, password))
+    con.commit()
+    con.close()
     return True
   except IntegrityError:
     return False
+
+
+def existingUser(username):
+  con = sqlite3.connect("users.db")
+  cur = con.cursor()
+
+  username = cur.execute("""
+    select * from Users where Username = ?
+  """, (username, )).fetchone()
+  if username == None:
+    return False
+  return True
+
+
+def validLogin(username, password):
+  con = sqlite3.connect("users.db")
+  cur = con.cursor()
+
+  valid = cur.execute("""
+    select * from Users where Username = ? and Password = ?
+  """, (username, password)).fetchone()
+  if valid == None:
+    return False
+  return True
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
 from random import choice
-
+import database
+from helpers import login_required
+from tempfile import mkdtemp
 app = Flask(__name__)
 
 number_list = [
@@ -8,52 +11,84 @@ number_list = [
 	416, 417, 418, 421, 422, 423, 424, 425, 426,
 	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
 ]
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-# W userlogin done already
-
 @app.route('/calendar')
+# @login_required
 def calendar():
   return render_template("calendar.html")
 
 
 @app.route("/dailytasks", methods=["POST", "GET"])
+# @login_required
 def dailytasks():
   return render_template("dailytasks.html")
 
-@app.route("/dailyemotions")
-def dailyemotions(methods=["POST"]):
+@app.route("/dailyemotions", methods=["POST", "GET"])
+# @login_required
+def dailyemotions():
   return render_template("dailyemotions.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+  session.clear()
   if request.method == "POST":
     if not request.form.get("username"):
       return render_template("error.html", error="You must enter a valid username")
     if not request.form.get("password"):
       return render_template("error.html", error="You must enter a valid password")
     username = request.form.get("username")
-    print(username)
-    return render_template("index.html")
-
+    password = request.form.get("password")
+    if database.validLogin(username, password) == True:
+      session["user_id"] = username
+      print(session)
+      return render_template("index.html")
+    return render_template("error.html", error = "This is not a valid login")
 
   return render_template("login.html")
 
 
 @app.route("/gethelp", methods=["POST", "GET"])
+# @login_required
 def gethelp():
   return render_template("gethelp.html")
 
-@app.route("/create")
-def create(methods=["POST", "GET"]):
+@app.route("/create", methods=["POST", "GET"])
+def create():
+  if request.method == "POST":
+    if not request.form.get("username"):
+      return render_template("error.html", error = "You must enter a username")
+    if not request.form.get("password"):
+      return render_template("error.html", error = "You must enter a password")
+    username = request.form.get("username")
+    if database.existingUser(username) == True:
+      return render_template("error.html", error = "This username is taken")
+    password = request.form.get("password")
+    password2 = request.form.get("password2")
+    if password == password2:
+      database.createUser(username, password)
+      return render_template("login.html")
+    return render_template("error.html", error="Passwords must match")
   return render_template("create.html")
-
-
-
-
 
 
 
